2c4d0c1054609d4e59a53c9e735ce1bcec058e22
Alex Mitzman and Olga Talovikova <pair+alex+olga@allstate.com>
2016-08-10 16:08:15 -0500

    add test

---
+++ test/java/com/compozed/models/ClientTest.java
@@ -0,0 +1,35 @@
+package com.compozed.models;
+
+import com.compozed.util.Mysql;
+import org.hibernate.Session;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by localadmin on 8/10/16.
+ */
+public class ClientTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void shouldCreateNewClientAndSave() throws Exception {
+        Session session = Mysql.getSession();
+        session.beginTransaction();
+        Client c = new Client("jennifer");
+        session.save(c);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+}
